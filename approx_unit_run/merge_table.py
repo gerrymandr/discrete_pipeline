dca65354957378c1e988bd2825850bcc69b45048
Adriana Rogers <rogers.adriana@gmail.com>
2018-07-18 13:56:36 -0400

    Merged tables for bg and tract into tables_merged directory. Added continuous polsby popper.

--- approx_unit_run/merge_table.py
+++ approx_unit_run/merge_table.py
@@ -1,43 +1,19 @@
+import math
 import pandas as pd
 import glob
 '''Takes all of the files in ./tables/ and merges them into a csv, removing the headers, and saves new csv'''
 
-interesting_files = glob.glob("./tables/tigerline*tract.csv")
-files = [pd.read_csv(f, header = 0, converters={'geoid': lambda x: str(x)}) for f in sorted(interesting_files)]
-df = pd.concat(files, ignore_index = True)
-df.to_csv("tigerline_tract.csv")
+unit_name = ["tract", "bg"]
+district_files = ["tigerline", "cb500k", "cb5m", "cb20m"]
 
-interesting_files = glob.glob("./tables/tigerline*bg.csv")
-files = [pd.read_csv(f, header = 0, converters={'geoid': lambda x: str(x)}) for f in sorted(interesting_files)]
-df = pd.concat(files, ignore_index = True)
-df.to_csv("tigerline_bg.csv")
+for unit in unit_name:
+    for shape in district_files:
+        interesting_files = glob.glob("./tables/" + shape + "*" + unit + ".csv")
+        files = [pd.read_csv(f, header = 0, converters={'geoid': lambda x: str(x)}) for f in sorted(interesting_files)]
+        df = pd.concat(files, ignore_index = True)
 
-interesting_files = glob.glob("./tables/cb500k*tract.csv")
-files = [pd.read_csv(f, header = 0, converters={'geoid': lambda x: str(x)}) for f in sorted(interesting_files)]
-df = pd.concat(files, ignore_index = True)
-df.to_csv("cb500k_tract.csv")
+        # to add continuous polsby popper:
+        df["cpolsbypop"] = (4 * math.pi * df["carea"]) / (df["cperim"] * df["cperim"])
+        df = df[['geoid', 'carea', 'cperim', 'cpolsbypop', 'dperim_0.5', 'dpperim_0.5', 'dperim_pro_0.5', 'dpperim_pro_0.5', 'darea_0.5', 'dparea_0.5', 'darea_pro_0.5', 'dparea_pro_0.5', 'dperim_0.1', 'dpperim_0.1', 'dperim_pro_0.1', 'dpperim_pro_0.1', 'darea_0.1', 'dparea_0.1', 'darea_pro_0.1', 'dparea_pro_0.1']]
+        df.to_csv("./tables_merged/" + shape + "_" + unit + ".csv")
 
-interesting_files = glob.glob("./tables/cb500k*bg.csv")
-files = [pd.read_csv(f, header = 0, converters={'geoid': lambda x: str(x)}) for f in sorted(interesting_files)]
-df = pd.concat(files, ignore_index = True)
-df.to_csv("cb500k_bg.csv")
-
-interesting_files = glob.glob("./tables/cb5m*tract.csv")
-files = [pd.read_csv(f, header = 0, converters={'geoid': lambda x: str(x)}) for f in sorted(interesting_files)]
-df = pd.concat(files, ignore_index = True)
-df.to_csv("cb5m_tract.csv")
-
-interesting_files = glob.glob("./tables/cb5m*bg.csv")
-files = [pd.read_csv(f, header = 0, converters={'geoid': lambda x: str(x)}) for f in sorted(interesting_files)]
-df = pd.concat(files, ignore_index = True)
-df.to_csv("cb5m_bg.csv")
-
-interesting_files = glob.glob("./tables/cb20m*tract.csv")
-files = [pd.read_csv(f, header = 0, converters={'geoid': lambda x: str(x)}) for f in sorted(interesting_files)]
-df = pd.concat(files, ignore_index = True)
-df.to_csv("cb20m_tract.csv")
-
-interesting_files = glob.glob("./tables/cb20m*bg.csv")
-files = [pd.read_csv(f, header = 0, converters={'geoid': lambda x: str(x)}) for f in sorted(interesting_files)]
-df = pd.concat(files, ignore_index = True)
-df.to_csv("cb20m_bg.csv")
