efe0b4a1e4c3c2aecb8de19daece0b56943213e2
Adriana Rogers <rogers.adriana@gmail.com>
2018-07-18 15:25:28 -0400

    Added discrete scores and rankings.

--- approx_unit_run/merge_table.py
+++ approx_unit_run/merge_table.py
@@ -13,7 +13,20 @@ for unit in unit_name:
         df = pd.concat(files, ignore_index = True)
 
         # to add continuous polsby popper:
-        df["cpolsbypop"] = (4 * math.pi * df["carea"]) / (df["cperim"] * df["cperim"])
-        df = df[['geoid', 'carea', 'cperim', 'cpolsbypop', 'dperim_0.5', 'dpperim_0.5', 'dperim_pro_0.5', 'dpperim_pro_0.5', 'darea_0.5', 'dparea_0.5', 'darea_pro_0.5', 'dparea_pro_0.5', 'dperim_0.1', 'dpperim_0.1', 'dperim_pro_0.1', 'dpperim_pro_0.1', 'darea_0.1', 'dparea_0.1', 'darea_pro_0.1', 'dparea_pro_0.1']]
+        df["cpolsby"] = (4 * math.pi * df["carea"]) / (df["cperim"] * df["cperim"])
+
+        percent_list = ["0.5", "0.1"]
+        for percent in percent_list:
+            df["dpolsby_" + percent] = (df["darea_" + percent]) / (df["dperim_" + percent] * df["dperim_" + percent])
+            df["dpolsby_pro_" + percent] = (df["darea_pro_" + percent]) / (df["dperim_pro_" + percent] * df["dperim_pro_" + percent])
+            df["dpop_polsby_" + percent] = (df["dparea_" + percent]) / (df["dpperim_" + percent] * df["dpperim_" + percent])
+            df["dpop_polsby_pro_" + percent] = (df["dparea_pro_" + percent]) / (df["dpperim_pro_" + percent] * df["dpperim_pro_" + percent])
+
+            df["rank_dpolsby_" + percent] = df["dpolsby_" + percent].rank()
+            df["rank_dpolsby_pro_" + percent] = df["dpolsby_pro_" + percent].rank()
+            df["rank_dpop_polsby_" + percent] = df["dpop_polsby_" + percent].rank()
+            df["rank_dpop_polsby_pro_" + percent] = df["dpop_polsby_pro_" + percent].rank()
+
+        #df = df[['geoid', 'carea', 'cpolsbypop', 'dperim_0.5', 'dpperim_0.5', 'dperim_pro_0.5', 'dpperim_pro_0.5', 'darea_0.5', 'dparea_0.5', 'darea_pro_0.5', 'dparea_pro_0.5', 'dperim_0.1', 'dpperim_0.1', 'dperim_pro_0.1', 'dpperim_pro_0.1', 'darea_0.1', 'dparea_0.1', 'darea_pro_0.1', 'dparea_pro_0.1', 'cpolsby', 'dpolsby_0.5', 'dpolsby_01', 'dpolsby_pro_0.5', 'dpolsby_pro_0.1', 'dpop_polsby_0.5', 'dpop_polsby_0.1', 'dpop_polsby_pro_0.5', 'dpop_polsby_pro_0.1']]
         df.to_csv("./tables_merged/" + shape + "_" + unit + ".csv")
 
