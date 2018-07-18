ae5bd39844fbf239425ce6038aae6671bff6f46d
AssafBarNaatan <assafbarnatan@gmail.com>
2018-07-18 17:44:30 -0400

    IDK, Adriana wanted this

--- approx_unit_run/merge_table.py
+++ approx_unit_run/merge_table.py
@@ -34,23 +34,3 @@ for unit in unit_name:
         #df.to_csv("./tables_merged/" + shape + "_" + unit + ".csv")
 
 
-unit = "bg"
-file = "tigerline"
-#plot_df = pd.read_csv("./tables_merged/" + file + "_" + unit + ".csv")
-#plot_df.plot(x="rank_dpolsby_0.1", y="rank_dpolsby_pro_0.1", 
-#             title = "Scores for " + unit + "'s from: " + file, 
-#             kind = 'scatter')
-
-def myplot(xtable_name, ytable_name, xvec, yvec):
-    df1 = pd.read_csv("./tables_merged/" + xtable_name)
-    df2 = pd.read_csv("./tables_merged/" + ytable_name)
-    x = df1[xvec]
-    y = df1[yvec]
-    plt.scatter(x,y)
-    plt.title("Scatter plot of " + xvec + " and " + yvec)
-    plt.ylabel(ytable_name + " : " + yvec)
-    plt.xlabel(xtable_name + " : " + xvec)
-
-myplot("tigerline_bg.csv", "tigerline_tract.csv", "rank_cpolsby", "rank_cpolsby")
-#plt.figure
-#plt.scatter(x,y)
--- approx_unit_run/table_analysis.py
+++ approx_unit_run/table_analysis.py
@@ -4,6 +4,8 @@ Created on Wed Jul 18 16:24:29 2018
 
 @author: assaf, adriana
 """
+import pandas as pd
+import matplotlib.pyplot as plt
 
 unit = "bg"
 file = "tigerline"
@@ -12,16 +14,31 @@ file = "tigerline"
 #             title = "Scores for " + unit + "'s from: " + file, 
 #             kind = 'scatter')
 
-def myplot(xtable_name, ytable_name, xvec, yvec):
-    df1 = pd.read_csv("./tables_merged/" + xtable_name)
-    df2 = pd.read_csv("./tables_merged/" + ytable_name)
-    x = df1[xvec]
-    y = df1[yvec]
+def different_table_plot(xtable_name, ytable_name, xvec, yvec):
+    #xtable_name = "cb20m_bg.csv"
+    #ytable_name = "cb5m_bg.csv"
+    #xvec = "rank_cpolsby"
+    #yvec = "rank_cpolsby"
+    
+    df1 = pd.read_csv("./tables_merged/" + xtable_name, dtype={"geoid":str})
+    df2 = pd.read_csv("./tables_merged/" + ytable_name, dtype={"geoid":str})
+    
+    xdf = df1["geoid"].to_frame()
+    xdf["xname"] = df1[xvec]
+    
+    ydf = df2["geoid"].to_frame()
+    ydf["yname"] = df2[yvec]
+    
+    mergedf = xdf.merge(ydf, left_on="geoid", right_on="geoid")
+    
+    x = mergedf["xname"]
+    y = mergedf["yname"]
+    
     plt.scatter(x,y)
     plt.title("Scatter plot of " + xvec + " and " + yvec)
     plt.ylabel(ytable_name + " : " + yvec)
     plt.xlabel(xtable_name + " : " + xvec)
 
-myplot("cb20m_bg.csv", "tigerline_bg.csv", "rank_cpolsby", "rank_dpolsby_0.1")
+different_table_plot("cb500k_bg.csv", "tigerline_bg.csv", "rank_cpolsby", "rank_cpolsby")
 #plt.figure
 #plt.scatter(x,y)
