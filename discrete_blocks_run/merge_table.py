044852b96a406faabab0463909c53769ea72548a
Adriana Rogers <rogers.adriana@gmail.com>
2018-07-19 15:16:40 -0400

    Created the big tables in analysis.

--- discrete_blocks_run/merge_table.py
+++ discrete_blocks_run/merge_table.py
@@ -5,4 +5,18 @@ import glob
 interesting_files = glob.glob("./tables/*.csv")
 files = [pd.read_csv(f, header = 0, converters={'geoid': lambda x: str(x)}) for f in sorted(interesting_files)]
 df = pd.concat(files, ignore_index = True)
+
+percent_list = ["0.5", "0.1"]
+
+for perc in percent_list:
+    df["a/p^2 (b) " + perc] = (df["darea"]) / (df["dperim"] * df["dperim"])
+    df["w_a/p^2 (b) " + perc] = (df["dparea"]) / (df["dpperim"] * df["dpperim"])
+    df["rank_a/p^2 (b) " + perc] = df["a/p^2 (b) " + perc].rank()
+    df["rank_w_a/p^2 (b) " + perc] = df["w_a/p^2 (b) " + perc].rank()
+
+df = df.rename(columns={'dperim': 'perim (b)', 'dpperim': 'w_perim (b)', 
+                        'darea': 'area (b)', 'dparea': 'w_area (b)'})
+
 df.to_csv("merged_blocks.csv")
+
+
