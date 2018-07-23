5ffd169b7e1c0c2a492d79b5d257fc498486a98f
AssafBarNaatan <assafbarnatan@gmail.com>
2018-07-23 10:25:42 -0400

    Happy Monday

--- analysis/table_analysis.py
+++ analysis/table_analysis.py
@@ -10,7 +10,6 @@ import matplotlib.pyplot as plt
 from descartes import PolygonPatch
 import scipy.stats as stats
 
-
 # create a dictionary of FIPS code : state name for titles
 fips = pd.read_csv('../state_fips.txt', sep='\t', lineterminator='\n',
                    dtype={"STATE": str, "FIP": str})
@@ -43,9 +42,9 @@ def different_table_plot(xtable_name, ytable_name, xvec, yvec):
 def find_subplot(event):
     mouse = event.mouseevent
     (fx, fy) = f.transFigure.inverted().transform((mouse.x, mouse.y))
-    bb1 = ax1.get_position()
-    bb2 = ax2.get_position()
-    bb3 = ax3.get_position()
+    bb1 = axarr[0,0].get_position()
+    bb2 = axarr[0,1].get_position()
+    bb3 = axarr[1,0].get_position()
     if bb1.contains(fx, fy):
         return 1
     if bb2.contains(fx, fy):
@@ -65,16 +64,16 @@ def highlight_event(event, plotnum):
         selected_geoid = geoids3[ind]
 
     ind1 = geoids1.index(selected_geoid)
-    selected1.set_visible(True)
-    selected1.set_data([x1[ind1]], [y1[ind1]])
+    selected[0].set_visible(True)
+    selected[0].set_data([x1[ind1]], [y1[ind1]])
 
     ind2 = geoids2.index(selected_geoid)
-    selected2.set_visible(True)
-    selected2.set_data([x2[ind2]], [y2[ind2]])
+    selected[1].set_visible(True)
+    selected[1].set_data([x2[ind2]], [y2[ind2]])
 
     ind3 = geoids3.index(selected_geoid)
-    selected3.set_visible(True)
-    selected3.set_data([x3[ind3]], [y3[ind3]])
+    selected[2].set_visible(True)
+    selected[2].set_data([x3[ind3]], [y3[ind3]])
     return selected_geoid
 
 
@@ -98,7 +97,7 @@ def on_pick(event):
 def drawmap(geoid, df):
     # create a new figure
     # fig2, ax2 = plt.subplots()
-    ax4.clear()
+    axarr[1,1].clear()
     patchdict = {}
     for i, dist in df.iterrows():
         if dist["GEOID"] == geoid:
@@ -110,71 +109,77 @@ def drawmap(geoid, df):
             patch = PolygonPatch(dist.geometry, fc='#D3D3D3',
                                  ec='#D3D3D3', alpha=1, zorder=2)
             patchdict.update({i: patch})
-            ax4.add_patch(patch)
-            ax4.axis('scaled')
-    ax4.add_patch(special_patch)
-    ax4.axis('scaled')
-    ax4.set_title("district " + str(int(geoid[2:])) + " in " +
+            axarr[1,1].add_patch(patch)
+            axarr[1,1].axis('scaled')
+    axarr[1,1].add_patch(special_patch)
+    axarr[1,1].axis('scaled')
+    axarr[1,1].set_title("district " + str(int(geoid[2:])) + " in " +
                   fips_dict[geoid[:2]])
     plt.draw()
 
 
-f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+f, axarr = plt.subplots(2, 2)
+selected = []
 
-
-xvec = "500k_rank_4pi*a/p^2"
+xvec = "rank_a/p^2 (b) 0.5"
 yvec = "rank_a/p^2 (g) 0.5"
-xtable_name = "zoom_table.csv"
-ytable_name = "zoom_table.csv"
+xtable_name = "big_table.csv"
+ytable_name = "big_table.csv"
 
+# xvec = "rank_tiger_aland"
+# yvec = "tiger_rank_c_4pi*a/p^2"
+# xtable_name = "zoom_table.csv"
+# ytable_name = "zoom_table.csv"
 (x1, y1, geoids1) = different_table_plot(xtable_name, ytable_name,
                                          xvec, yvec)
-selected1 = ax1.plot(0, 0, 'o', ms=12, alpha=1, color='red', visible=False)[0]
+selected.extend(axarr[0,0].plot(0, 0, 'o', ms=12, alpha=1, color='red', visible=False))
 
-ax1.plot(x1, y1, 'o', picker=10, color='purple')
-ax1.set_title("Scatter plot of " + xvec + " and " + yvec)
-ax1.set_ylabel(ytable_name + " : " + yvec)
-ax1.set_xlabel(xtable_name + " : " + xvec)
+axarr[0,0].plot(x1, y1, 'o', picker=10, color='purple')
+axarr[0,0].set_title("Scatter plot of " + xvec + " and " + yvec)
+axarr[0,0].set_ylabel(ytable_name + " : " + yvec)
+axarr[0,0].set_xlabel(xtable_name + " : " + xvec)
 
 tau, p_value = stats.kendalltau(x1, y1)
 props = dict(boxstyle='round', facecolor='wheat', alpha=0.9)
-ax1.text(0.05, 0.95, str(tau)[:6], transform=ax1.transAxes, fontsize=14, 
+axarr[0,0].text(0.05, 0.95, str(tau)[:6], transform=axarr[0,0].transAxes, fontsize=14, 
         verticalalignment='top', bbox=props)
 
 xvec = "rank_a/p^2 (g) 0.5"
 yvec = "rank_a/p^2 (t) 0.5"
-(x2, y2, geoids1) = different_table_plot(xtable_name, ytable_name,
+xtable_name = "big_table.csv"
+ytable_name = "big_table.csv"
+(x2, y2, geoids2) = different_table_plot(xtable_name, ytable_name,
                                          xvec, yvec)
-selected2 = ax2.plot(0, 0, 'o', ms=12, alpha=1, color='red', visible=False)[0]
+selected.extend(axarr[0,1].plot(0, 0, 'o', ms=12, alpha=1, color='red', visible=False))
 
-ax2.plot(x2, y2, 'o', picker=10, color='purple')
-ax2.set_title("Scatter plot of " + xvec + " and " + yvec)
-ax2.set_ylabel(ytable_name + " : " + yvec)
-ax2.set_xlabel(xtable_name + " : " + xvec)
+axarr[0,1].plot(x2, y2, 'o', picker=10, color='purple')
+axarr[0,1].set_title("Scatter plot of " + xvec + " and " + yvec)
+axarr[0,1].set_ylabel(ytable_name + " : " + yvec)
+axarr[0,1].set_xlabel(xtable_name + " : " + xvec)
 
 tau, p_value = stats.kendalltau(x2, y2)
 props = dict(boxstyle='round', facecolor='wheat', alpha=0.9)
-ax2.text(0.05, 0.95, str(tau)[:6], transform=ax2.transAxes, fontsize=14, 
+axarr[0,1].text(0.05, 0.95, str(tau)[:6], transform=axarr[0,1].transAxes, fontsize=14, 
         verticalalignment='top', bbox=props)
 
 xvec = "rank_a/p^2 (t) 0.5"
 yvec = "rank_a/p^2 (b) 0.5"
-(x3, y3, geoids1) = different_table_plot(xtable_name, ytable_name,
+(x3, y3, geoids3) = different_table_plot(xtable_name, ytable_name,
                                          xvec, yvec)
-selected3 = ax3.plot(0, 0, 'o', ms=12, alpha=1, color='red', visible=False)[0]
+selected.extend(axarr[1, 0].plot(0, 0, 'o', ms=12, alpha=1, color='red', visible=False))
 
-ax3.plot(x3, y3, 'o', picker=10, color='purple')
-ax3.set_title("Scatter plot of " + xvec + " and " + yvec)
-ax3.set_ylabel(ytable_name + " : " + yvec)
-ax3.set_xlabel(xtable_name + " : " + xvec)
+axarr[1, 0].plot(x3, y3, 'o', picker=10, color='purple')
+axarr[1, 0].set_title("Scatter plot of " + xvec + " and " + yvec)
+axarr[1, 0].set_ylabel(ytable_name + " : " + yvec)
+axarr[1, 0].set_xlabel(xtable_name + " : " + xvec)
 
 tau, p_value = stats.kendalltau(x3, y3)
 props = dict(boxstyle='round', facecolor='wheat', alpha=0.9)
-ax3.text(0.05, 0.95, str(tau)[:6], transform=ax3.transAxes, fontsize=14, 
+axarr[1,0].text(0.05, 0.95, str(tau)[:6], transform=axarr[1,0].transAxes, fontsize=14, 
         verticalalignment='top', bbox=props)
 '''
-xvec = "rank_tiger_aland"
-yvec = "tiger_rank_c_4pi*a/p^2"
+xvec = "tiger_rank_c_4pi*a/p^2"
+yvec = "20m_rank_c_4pi*a/p^2"
 (x4, y4, geoids1) = different_table_plot(xtable_name, ytable_name,
                                          xvec, yvec)
 selected4 = ax4.plot(0, 0, 'o', ms=12, alpha=1, color='red', visible=False)[0]
@@ -186,9 +191,9 @@ ax4.set_xlabel(xtable_name + " : " + xvec)
 
 tau, p_value = stats.kendalltau(x4, y4)
 props = dict(boxstyle='round', facecolor='wheat', alpha=0.9)
-ax4.text(0.05, 0.95, str(tau)[:6], transform=ax3.transAxes, fontsize=14, 
+ax4.text(0.05, 0.95, str(tau)[:6], transform=ax4.transAxes, fontsize=14, 
         verticalalignment='top', bbox=props)
 '''
 f.canvas.mpl_connect('pick_event', on_pick)
-plt.suptitle("Comparing discrete Polsby Popper scores across unit resolutions (with normalized Kendall tau scores)", 
+plt.suptitle("Comparing Polsby Popper scores across cartographic boundary resolutions (with normalized Kendall tau scores)", 
              fontsize = 22)
