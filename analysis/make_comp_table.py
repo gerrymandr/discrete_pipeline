6eb3e08d9bb4200fd15c219524a272e80670913a
Adriana Rogers <rogers.adriana@gmail.com>
2018-07-26 10:36:44 -0400

    Adding ranked perim columns.

--- analysis/make_comp_table.py
+++ analysis/make_comp_table.py
@@ -14,29 +14,20 @@ df = df.merge(zoom, left_on = "geoid", right_on = "geoid")
 df = df.drop(['Unnamed: 0_x', 'Unnamed: 0_y', 'cont_perim', 'cont_area', 'cont_pp', 'rank_cont_pp', 'state_y'], axis=1)
 df = df.rename(columns={'state_x': 'state'})
 
-print(list(df.columns))
-
-#for unit in ['b_', 'g_', 't_']:
-#    for perc in ['0.1', '0.5']:
-#        for weight in ['w_', '']:
-#            for pro in ['pro_', '']:
-#                if (pro == 'pro_' and unit == 'b_'):
-#                    break
-#                else:
-#                    if pro + "disc_" + weight + 'perim_' + unit + perc not in df:
-#                        df["rank_" + pro + "disc_" + weight + 'perim_' + unit + perc] = df[
-#                                pro + "disc_" + weight + 'perim_' + unit + perc].rank(ascending = False)
+#print(list(df.columns))
 
 for unit in ['b_', 'g_', 't_']:
     for perc in ['0.1', '0.5']:
         for weight in ['w_', '']:
             df["rank_disc_" + weight + "area_" + unit + perc] = df["disc_" + weight + "area_" + unit + perc].rank(ascending = False)
+            df["rank_disc_" + weight + "perim_" + unit + perc] = df["disc_" + weight + "perim_" + unit + perc].rank(ascending = False)
 
 for unit in ['g_', 't_']:
     for perc in ['0.1', '0.5']:
         for weight in ['w_', '']:
             for pro in ['pro_', '']:
                 df["rank_" + pro + "disc_" + weight + "area_" + unit + perc] = df[pro + "disc_" + weight + "area_" + unit + perc].rank(ascending = False)
+                df["rank_" + pro + "disc_" + weight + "perim_" + unit + perc] = df[pro + "disc_" + weight + "perim_" + unit + perc].rank(ascending = False)
 
 df.to_csv("./comp_table.csv")      # length 54
 print(len(list(df.columns)))
